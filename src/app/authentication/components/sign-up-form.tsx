4ff09bad5d8e24e76b456e484505a78b377e7e81
moisdutra <moisesbrdg3@hotmail.com>
2025-05-29 00:53:06 -0300

    feat: add custom error message for existing user

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -2,6 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -59,6 +60,15 @@ const SignUpForm = () => {
       {
         onSuccess: () => {
           router.push("/dashboard");
+        },
+        onError: (ctx) => {
+          console.log(ctx.error);
+          if (ctx.error.code === "USER_ALREADY_EXISTS") {
+            toast.error("Email já cadastrado");
+            return;
+          }
+          // toast.error(ctx.error.message);
+          toast.error("Erro ao criar conta");
         },
       },
     );
