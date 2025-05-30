fdfcdfe67259c28fc4266892aaff0e8ad85a9a81
moisdutra <moisesbrdg3@hotmail.com>
2025-05-30 19:52:39 -0300

    feat: create page container components

---
+++ src/app/(protected)/doctors/page.tsx
@@ -0,0 +1,34 @@
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+
+const DoctorsPage = () => {
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Plus />
+          <Button>Adicionar médico</Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <h1>Médicos</h1>
+      </PageContent>
+    </PageContainer>
+  );
+};
+
+export default DoctorsPage;
