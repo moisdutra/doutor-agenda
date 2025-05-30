65e2218f7f7b0b92440fba70135b289a9da99812
moisdutra <moisesbrdg3@hotmail.com>
2025-05-29 22:06:28 -0300

    feat: add clinic form page

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,9 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+// Import schema from schema.ts to be used in drizzle. Allowing to use the schema in the database.
+import * as schema from "./schema";
+
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema, // Allowing to use the schema in the database.
+});
